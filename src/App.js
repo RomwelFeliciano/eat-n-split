1f71967fe548a24fb399031047f9c50e342e54b4
Romwel Feliciano <127919521+RomwelFeliciano@users.noreply.github.com>
2023-09-30 16:11:52 +0800

    Updated if friendslist is empty

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import { useState } from "react";
 
 const initialFriends = [
-  {
-    id: 118836,
-    name: "Clark",
-    image: "https://i.pravatar.cc/48?u=118836",
-    balance: -7,
-  },
-  {
-    id: 933372,
-    name: "Sarah",
-    image: "https://i.pravatar.cc/48?u=933372",
-    balance: 20,
-  },
-  {
-    id: 499476,
-    name: "Anthony",
-    image: "https://i.pravatar.cc/48?u=499476",
-    balance: 0,
-  },
+  // {
+  //   id: 118836,
+  //   name: "Clark",
+  //   image: "https://i.pravatar.cc/48?u=118836",
+  //   balance: -7,
+  // },
+  // {
+  //   id: 933372,
+  //   name: "Sarah",
+  //   image: "https://i.pravatar.cc/48?u=933372",
+  //   balance: 20,
+  // },
+  // {
+  //   id: 499476,
+  //   name: "Anthony",
+  //   image: "https://i.pravatar.cc/48?u=499476",
+  //   balance: 0,
+  // },
 ];
 
 function Button({ children, onClick }) {
@@ -90,14 +90,18 @@ export default function App() {
 function FriendsList({ friends, selectedFriend, onSelection }) {
   return (
     <ul>
-      {friends.map((friend) => (
-        <Friend
-          friend={friend}
-          key={friend.id}
-          selectedFriend={selectedFriend}
-          onSelection={onSelection}
-        />
-      ))}
+      {Object.keys(friends).length === 0 ? (
+        <h1>There are no Friends</h1>
+      ) : (
+        friends.map((friend) => (
+          <Friend
+            friend={friend}
+            key={friend.id}
+            selectedFriend={selectedFriend}
+            onSelection={onSelection}
+          />
+        ))
+      )}
     </ul>
   );
 }
